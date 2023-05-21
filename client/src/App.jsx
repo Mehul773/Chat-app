218ec17511c379c8e3b4777ac06731ea8af657b7
mehul773 <chovatiyamehul2003@gmail.com>
2023-05-21 16:39:36 +0530

    Header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,24 @@
-import {Route, Routes} from 'react-router-dom'
-import IndexPage from './Pages/IndexPage';
-import LoginPage from './Pages/LoginPage';
-import RegisterPage from './Pages/RegisterPage';
-import axios from 'axios';
+import { Route, Routes } from "react-router-dom";
+import IndexPage from "./Pages/IndexPage";
+import LoginPage from "./Pages/LoginPage";
+import RegisterPage from "./Pages/RegisterPage";
+import axios from "axios";
+import Header from "./component/Header";
+import Layout from "./component/Layout";
 
-axios.defaults.baseURL = "http://localhost:4001/"
+axios.defaults.baseURL = "http://localhost:4001/";
 axios.defaults.withCredentials = true;
 //vKyW6mvMtyU7B2q4
 
 function App() {
-
   return (
     <>
       <Routes>
-        <Route path='/' element={<IndexPage/>}/>
-        <Route path='/login' element={<LoginPage/>}/>
-        <Route path='/register' element={<RegisterPage/>}/>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<IndexPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+        </Route>
       </Routes>
     </>
   );
